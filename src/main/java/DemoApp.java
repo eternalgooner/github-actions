8a49fedc6fd579589da529e49fc09589446a8841
David Mackessy <davidmackessy@Davids-MBP.lan>
2022-06-23 19:59:39 +0100

    add another line

--- src/main/java/DemoApp.java
+++ src/main/java/DemoApp.java
@@ -1,9 +1,11 @@
 public class DemoApp {
 
     public static void main(String[] args) {
-        System.out.println("gitgub actions");
+        System.out.println("github actions");
 
-        System.out.println("gitgub actions");
+        System.out.println("github actions");
+        System.out.println("github actions");
+        System.out.println("github actions");
 
 
     }
